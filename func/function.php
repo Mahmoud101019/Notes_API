f776adb14d0b6569464a12a2fe9f7fcb16b5c108
ss <mahmoudmagdy101019@gmail.com>
2024-08-30 14:46:07 +0300

    Create Function of upload file

--- func/function.php
+++ func/function.php
@@ -1,7 +1,31 @@
 <?php
-
+   define("MB" , 1048576);
      function filterRequest($requestname){
         return  htmlspecialchars(strip_tags($_POST[$requestname]));
+     }
+
+
+
+     function imageUpload($imagerequest){
+      global $msgError;
+      $imagename = $_FILES[$imagerequest]['name'];
+      $imagetmp = $_FILES[$imagerequest]['tmp_name'];
+      $imagesize = $_FILES[$imagerequest]['size'];
+      $allowtExt = array("jpg", "gif" ,"png" , "pdf" , "mp3");
+      $strToArray = explode("." , $imagename);
+      $ext = end($strToArray);
+      $ext = strtolower($ext);
+      if (!empty($imagename) && !in_array($ext , $allowtExt) ) {
+         $msgError[]= "Ext";
+      }
+      if ($imagesize > 2 *MB) {
+         $msgError[]= "Size";
+      }
+      if (empty($msgError)) {
+         move_uploaded_file($imagetmp,"upload/" . $imagename);
+      }else{
+         print_r($msgError);
+      }
      }
 
 ?>
