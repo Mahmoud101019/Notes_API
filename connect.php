a776b7a6ff55a885ede84d6540f39afdbb5baf02
ss <mahmoudmagdy101019@gmail.com>
2024-09-03 17:57:10 +0300

    Finished

--- connect.php
+++ connect.php
@@ -15,7 +15,14 @@
                $option
           ); 
           $con->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
+          
+          header("Access-Control-Allow-Origin: *");
+          header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
+          header("Access-Control-Allow-Methods: POST, OPTIONS , GET");
+
           include_once "func/function.php";
+
+          checkAuthenticate();
      } catch (PDOException $e) {
           //throw $e;
           echo $e->getMessage();
